a983a903c5951e91a221e217d5668d99b3c72371
NotMagical <NotMagicalDev@gmail.com>
2022-09-17 22:03:38 +0000

    Feature: base timer.

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,43 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div class="column items-center justify-center">
+      <q-circular-progress
+        reverse
+        :value="timer"
+        :min="0"
+        :max="maxRange"
+        size="200px"
+        :thickness="1"
+        color="grey-9"
+        track-color="light-blue"
+        class="q-ma-md cursor-pointer"
+        @click="startTimer"
+      />
+      <div class="row items-center q-gutter-xs">
+        <q-btn
+          label="Start"
+          color="green"
+          @click="startTimer()"
+          v-if="timer === 0"
+        />
+        <q-btn label="Remove" color="red" @click="removeTimer()" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const timer = ref(0);
+const maxRange = ref(1 * 60);
+const instance = ref<NodeJS.Timeout | number>(0);
+const addToTimer = () => timer.value++;
+const startTimer = () => {
+  instance.value = setInterval(addToTimer, 1000);
+};
+const removeTimer = () => {
+  clearInterval(instance.value);
+  timer.value = 0;
+  instance.value = 0;
+};
 </script>
